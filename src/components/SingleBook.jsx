91055dfe3f54477d955ea6ccda7449b7adb83524
jasko255 <jasko255@gmail.com>
2021-06-08 16:16:17 +0200

    hooks

---
+++ src/components/SingleBook.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+// import CommentArea from './CommentArea'
+
+const SingleBook = ({book, selectedBook, changeSelectedBook}) => {
+
+    // state = {
+    //     selected: false
+    // }
+
+    
+        return (
+            <>
+                <Card
+                    // onClick={() => this.setState({ selected: !this.state.selected })}
+                    onClick={() => changeSelectedBook(book.asin)}
+                    style={{ border: selectedBook === book.asin ? '3px solid red' : 'none' }}
+                >
+                    <Card.Img variant="top" src={book.img} />
+                    <Card.Body>
+                        <Card.Title style={{ color: 'black' }}>{book.title}</Card.Title>
+                    </Card.Body>
+                </Card>
+                {/* {
+                    this.state.selected && <CommentArea asin={this.props.book.asin} />
+                } */}
+            </>
+        )
+    
+
+}
+
+export default SingleBook
